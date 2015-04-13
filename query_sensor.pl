1d102367776d454a72531b5f2758f177a815c3fb
Mathew Richard Rumsey <mathew.rumsey@maine.edu>
2015-04-13 16:42:57 -0400

    Added query_sensor.pl.
    query_sensor.pl currently returns raw ADC value only.

---
+++ query_sensor.pl
@@ -0,0 +1,15 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+print get_raw_temp()."\n";
+
+sub get_raw_temp {
+	my $command = "sudo i2cget -y 1 0x41 6; sudo i2cget -y 1 0x41 7";
+	my @c_ret = `$command`;
+	return -1 if @c_ret != 2;
+	foreach my $line (@c_ret) {
+		return -2 if $line !~ s/^(0x\w\w)\n$/$1/;
+	}
+	return hex($c_ret[0])<<4 | hex($c_ret[1]);
+}
