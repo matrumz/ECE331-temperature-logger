d343b978f7b1e600e00811debb32a3fc3eaa4555
Mathew Richard Rumsey <mathew.rumsey@maine.edu>
2015-04-17 12:19:47 -0400

    Successfully pull temp.db data with PHP in index.php.
    Needed to move temp.db from GIT repo to /var/www to get around lighttpd root-locking.

--- index.php
+++ index.php
@@ -1,6 +1,31 @@
 <HTML>
 <TITLE>RUMSEY Project 2</TITLE>
 <BODY>
+<pre>
+<?php
+$database = "temp.db";
+$table = "T";
+$qry = "SELECT * FROM T ORDER BY Date DESC, Time DESC;";
+$data;
 
+# Connect to database and pull all data
+try {
+	# Connect
+	$dbh = new PDO("sqlite:$database");
+	# Set errormode to Exceptions
+	$dbh->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+	# Capture table data
+	$db_data = $dbh->query($qry);
+	#$dbh->closeCursor();
+	# Put into PHP array
+	$data = $db_data->fetchAll();
+} catch (PDOException $e) {
+	echo $e->getMessage()."\n";
+}
+$dbh = NULL;
+print_r($data);
+echo "WHY CAN'T NO ONE SEE ME?\n";
+?>
+</pre>
 </BODY>
 </HTML>
