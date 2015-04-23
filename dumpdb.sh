54373671aef5e17d955de1b2480687ea8c86d842
Mathew Richard Rumsey <mathew.rumsey@maine.edu>
2015-04-23 18:11:37 -0400

    UPDATED dumbdb.sh to reflect old change in location of temp.db.

--- dumpdb.sh
+++ dumpdb.sh
@@ -2,4 +2,4 @@
 # Creates a text output of Temperature Database temp.db
 # The output can also be used to restore temperature (T) table
 
-sqlite3 temp.db .dump > DBOUT
+sqlite3 /var/www/temp.db .dump > DBOUT
